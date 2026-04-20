70c673486eb8377ac23642ebfbac9c379c50ae5e
Debosmita Paul <debosmitapaul.19012005@gmail.com>
2026-04-20 23:26:23 +0530

    Render deployment fix

--- app.py
+++ app.py
@@ -373,4 +373,5 @@ def chart_data():
 
 if __name__ == '__main__':
     init_db()
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
